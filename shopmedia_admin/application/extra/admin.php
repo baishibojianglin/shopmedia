0e30a7d1a732955b9b1e666941ea869d67669b0e
Greetme <1980900198@qq.com>
2020-04-20 20:13:34 +0800

    admin: app/extra/admin.php

--- shopmedia_admin/application/extra/admin.php
+++ shopmedia_admin/application/extra/admin.php
@@ -6,9 +6,9 @@
  * Time: 18:51
  */
 
-// ����ϵͳ����
+// 管理系统配置
 return [
-    'login_time_out' => 3600 . 'seconds', // ��¼token��ʧЧʱ��
+    'login_time_out' => 3600 . 'seconds', // 登录token的失效时间
 
-    'platform_company_id' => 0 // ϵͳƽ̨��Ӧ�ķֹ�˾ID
+    'platform_company_id' => 0 // 系统平台对应的分公司ID
 ];
