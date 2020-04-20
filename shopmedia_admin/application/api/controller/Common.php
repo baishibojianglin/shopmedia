2bc759e04f07c0d0c124ce11f68315e0cc3caebc
Greetme <1980900198@qq.com>
2020-04-20 17:50:56 +0800

    admin: api/cont/Common

--- shopmedia_admin/application/api/controller/Common.php
+++ shopmedia_admin/application/api/controller/Common.php
@@ -2,6 +2,8 @@
 
 namespace app\api\controller;
 
+use app\common\lib\exception\ApiException;
+use app\common\lib\IAuth;
 use think\Cache;
 use think\Controller;
 
@@ -56,6 +58,23 @@ class Common extends Controller
         sign 加密：客户端工程师，解密：服务端工程师
         1.headers、body仿照sign形式做参数的加解密：如对 headers 的 version、apptype、did、model 加密放在一个参数 headers_params 里，对 headers_params 解密时则生成字符串 'version=1&apptype=android&did=12345dg&model=mix2s'
         2.IAuth()->setSign()的算法步骤需要客户端与服务端工程师约定，但最终算法是AES */
+
+        // 校验基础参数
+        if(empty($headers['sign'])){
+            throw new ApiException('sign不存在', 400);
+        }
+        if(!in_array($headers['apptype'], config('app.apptypes'))){
+            throw new ApiException('apptype不合法', 400);
+        }
+
+        // 校验sign的合法性
+        if(!IAuth::checkSignPass($headers)){
+            throw new ApiException('授权码sign校验失败', 401);
+        }
+
+        // 当sign校验成功时，存入缓存
+        // sign唯一性请求处理：①文件缓存，第一次请求后写标识（一台服务器） ②写入mysql（分布式） ③写入redis（分布式、数据量大）
+        Cache::set($headers['sign'], 1, config('app.app_sign_cache_time'));
 
         $this->headers = $headers; // headers信息校验成功后，便以其他继承该类的子类使用headers数据
     }
