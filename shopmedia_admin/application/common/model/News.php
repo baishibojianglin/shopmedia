c52192a8c5e18d929ca7bb7be42844e58253409b
Greetme <1980900198@qq.com>
2020-05-01 11:13:47 +0800

    news

---
+++ shopmedia_admin/application/common/model/News.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace app\common\model;
+
+use think\Model;
+
+/**
+ * 新闻模型类
+ * Class News
+ * @package app\common\model
+ */
+class News extends Base
+{
+    /**
+     * 获取新闻列表数据（基于paginate()自动化分页）
+     * @param array $map
+     * @param int $size
+     * @return \think\Paginator
+     */
+    public function getNews($map = [], $size = 5)
+    {
+        if(!isset($map['is_delete'])) {
+            $map['is_delete'] = ['neq', config('code.is_delete')];
+        }
+
+        $order = ['news_id' => 'asc'];
+
+        $result = $this->field('content', true) // 字段排除
+            ->where($map)
+            ->order($order)
+            ->paginate($size);
+        return $result;
+    }
+
+    /**
+     * 根据条件获取新闻列表数据
+     * @param array $map
+     * @param int $from
+     * @param int $size
+     * @return false|\PDOStatement|string|\think\Collection
+     */
+    public function getNewsByCondition($map = [], $from = 0, $size = 5)
+    {
+        if(!isset($map['is_delete'])) {
+            $map['is_delete'] = ['neq', config('code.is_delete')];
+        }
+
+        $order = ['news_id' => 'asc'];
+
+        $result = $this->where($map)
+            ->limit($from, $size)
+            ->order($order)
+            ->select();
+        //echo $this->getLastSql();
+        return $result;
+    }
+}
