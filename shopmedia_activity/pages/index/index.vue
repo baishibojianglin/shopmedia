1707558a8f29be616db59e11bf8f5a2a5868d834
jlnotebook <904880305@qq.com>
2020-06-23 14:25:38 +0800

    jl

---
+++ shopmedia_activity/pages/index/index.vue
@@ -0,0 +1,37 @@
+<template>
+	<view class="content">
+		<view class="text-area">
+			<text class="title">谢谢惠顾，下次再来</text>
+		</view>
+		<view class="bottom">店通智能屏&nbsp;&nbsp;&nbsp;&nbsp;智通天下市</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+			}
+		},
+		onLoad(){
+
+		},
+		methods:{
+
+		}
+	}
+</script>
+
+<style>
+	.content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+	.bottom{
+		position: fixed;
+		bottom: 30px;
+		font-size: 14px;
+	}
+</style>
