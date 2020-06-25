f492d8ffb6510f450f108750a880a05393f7e050
jlnotebook <904880305@qq.com>
2020-06-25 13:23:20 +0800

    jl

--- shopmedia_activity/pages/index/index.vue
+++ shopmedia_activity/pages/index/index.vue
@@ -1,7 +1,11 @@
 <template>
 	<view class="content">
-		<view class="text-area">
+		<view class="text-area" v-if="prize_no">
 			<text class="title">谢谢惠顾，下次再来</text>
+		</view>
+		
+		<view v-if="prize_yes">
+			
 		</view>
 		<view class="bottom">店通智能屏&nbsp;&nbsp;&nbsp;&nbsp;智通天下市</view>
 	</view>
@@ -11,15 +15,35 @@
 	export default {
 		data() {
 			return {
+				prize_no:false,
+				prize_yes:false
 			}
 		},
 		onLoad(option){
-            console.log(option.id)
+            //console.log(option.id)
+			this.prize(1);
 		},
 		methods:{
-             prize(){
-				 
+			
+			//调取奖品数据接口
+            prize(shop_id){	 
+				let self=this;
+				uni.request({
+						url: this.$serverUrl + 'api/get_prize',
+						data: {
+							shop_id:shop_id,
+						},
+						method: 'POST',
+						success: function(res) {
+							console.log(res)
+							if(res.data.status==0){ //未中奖
+								self.prize_no=true;
+							}
+						}
+				}) 			
 			 }
+			 
+			 
 		}
 	}
 </script>
