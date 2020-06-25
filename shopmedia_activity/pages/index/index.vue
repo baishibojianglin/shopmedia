43e8bd84dad4ca0c79aacb8945f312a285e46aed
jlnotebook <904880305@qq.com>
2020-06-25 22:53:31 +0800

    jl

--- shopmedia_activity/pages/index/index.vue
+++ shopmedia_activity/pages/index/index.vue
@@ -1,13 +1,24 @@
 <template>
 	<view class="content">
+		
+	<view v-show="showseconds">
+			<view style="width: 100%; margin-top: 30px;">
+			   <image style="width:80%;" :mode="mode" :src="src0"></image>	
+			</view>	
+			<view>
+				<text class="opentime">{{seconds}}</text>
+			</view>
+	</view>	
+		
+	<view v-show="!showseconds">
 		<view v-if="prize_no"><!--未中奖 s-->
 			<view style="width: 100%; margin-top: 30px;">
 			   <image style="width:100%;" :mode="mode" :src="src1"></image>	
 			</view>
-			<view style="margin-top:20px;">
-				<text class="color-shop">{{prize_info.shop_name}}</text>
+			<view style="margin-top:0px;">
+				<text class="color-shop" style="color:#007AFF; background-color: #fff; padding: 10px 40px; border-radius: 10px;">{{prize_info.shop_name}}</text>
 			</view>
-			<view style="margin-top:10px;">
+			<view style="margin-top:60px;">
 				<text>很遗憾未中奖，期待下次为您服务！</text>
 			</view>			
 		</view><!--未中奖 e-->
@@ -17,17 +28,17 @@
 			<view style="width: 100%; margin-top:20px;">
 			   <image style="width:90%;" :mode="mode" :src="src2"></image>	
 			</view>	
-			<view style="color:#f00;font-size: 35px; font-weight: bold;">{{prize_info.prize.prize_name}}</view>
+			<view style="color:#f00;font-size: 35px; font-weight: bold; position: relative; top:-15px;">{{prize_info.prize.prize_name}}</view>
 
 			<view style="margin-top: 20px;">
 				特别鸣谢
 				<br/>
-				<text style="font-weight: bold;">{{prize_info.prize.sponsor}}</text>
+				<text style="font-weight: bold;color:#007AFF; font-size: 22px; line-height: 50px;">{{prize_info.prize.sponsor}}</text>
 				<br/>
 				对该奖品的独家赞助
 			</view>
 
-<!-- 			<view style="margin-top: 30px; font-size: 16px;">
+            <!-- <view style="margin-top: 30px; font-size: 16px;">
 				感谢<text style="font-weight: bold;">{{prize_info.shop_name}}</text>大力支持
 			</view>	 -->				
 					
@@ -35,6 +46,11 @@
 				<button type="primary" style="width: 95%; margin-top:30px;">领取奖品</button>
 			</view>		
 		</view>
+	</view>	
+		
+		
+		
+		
 		<view class="bottom" style="width: 100%;">店通智能屏&nbsp;&nbsp;&nbsp;&nbsp;智通天下市</view>
 	</view>
 </template>
@@ -44,20 +60,45 @@
 		data() {
 			return {
 				mode: 'aspectFit',
+				src0: '/static/openprize.png',
 				src1: '/static/xxhg.png',
 				src2: '/static/gxzj.png',
 				prize_no:false,
 				prize_yes:false,
-				prize_info:{}
+				prize_info:{},
+				
+				seconds:5 ,//倒计时秒数
+				showseconds:true ,//显示倒计时
+				timesign:'' //定时器标志
 			}
 		},
 		onLoad(option){
             //console.log(option.id)
+			//倒计时
+			let self=this;
+			this.timesign=setInterval(function(){ self.countseconds(); },1000);
+			//获取奖品
 			this.prize(1);
+			
 		},
 		methods:{
-			
-			//调取奖品数据接口
+			/**
+			 * 倒计时函数
+			 */
+			countseconds() {
+			       if(this.seconds>0){
+					   this.seconds--;			   
+				   }else{
+					   this.showseconds=false;
+					   clearInterval(this.timesign);
+					   this.seconds=5;
+					   
+				   }
+			},
+			/**
+			 * 获取奖品
+			 * @param {Object} shop_id
+			 */
             prize(shop_id){	 
 				let self=this;
 				uni.request({
@@ -67,7 +108,6 @@
 						},
 						method: 'POST',
 						success: function(res) {
-							console.log(res)
 							if(res.data.status==0){ //未中奖
 								self.prize_no=true;
 								self.prize_info=res.data;
@@ -100,7 +140,18 @@
 		font-size: 14px;
 	}
 	.color-shop{
-		font-size: 18px;
+		font-size: 22px;
 		font-weight: bold;
+	}
+	.opentime{
+		display: inline-block;
+		height: 150px;
+		width: 150px;
+		line-height: 150px;
+		border-radius: 150px;
+		background-color: #E92F30;
+		color:#fff;
+		font-weight: bold;
+		font-size: 50px;
 	}
 </style>
