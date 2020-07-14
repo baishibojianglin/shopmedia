665dcb261859e6f52ed12c3211c76681f98ea224
jlnotebook <904880305@qq.com>
2020-07-14 10:56:18 +0800

    jl

--- shopmedia_partner_uniapp/main.js
+++ shopmedia_partner_uniapp/main.js
@@ -9,8 +9,8 @@ Vue.config.productionTip = false
 Vue.prototype.$store = store  //注入store
 Vue.prototype.$common = common
 // Vue.prototype.$serverUrl = 'http://media.sustock.local/index.php/' // 测试接口域名（本地）
-Vue.prototype.$serverUrl = 'http://media.dilinsat.com/index.php/' // 测试接口域名
-// Vue.prototype.$serverUrl = 'https://media.sustock.net/index.php/' // 正式接口域名
+//Vue.prototype.$serverUrl = 'http://media.dilinsat.com/index.php/' // 测试接口域名
+ Vue.prototype.$serverUrl = 'https://media.sustock.net/index.php/' // 正式接口域名
 Vue.prototype.$imgServerUrl = '' // 图片接口地址
 
 
