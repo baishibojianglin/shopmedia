b97db373bdbdb0a56b08ba8a9ec386eb6382228b
jlnotebook <904880305@qq.com>
2020-07-17 22:33:51 +0800

    jl

--- shopmedia_case/pages/index/index.vue
+++ shopmedia_case/pages/index/index.vue
@@ -19,30 +19,6 @@
 		},
 		onLoad(){
 			
-		   //获取屏幕高度和宽度
-		   
-		   var winWidth = 0;
-		   var winHeight = 0;
-			//获取窗口宽度
-			if (window.innerWidth)
-		   winWidth = window.innerWidth;
-			else if ((document.body) && (document.body.clientWidth))
-		   winWidth = document.body.clientWidth;
-		   //获取窗口高度
-			if (window.innerHeight)
-			winHeight = window.innerHeight;
-		   else if ((document.body) && (document.body.clientHeight))
-			winHeight = document.body.clientHeight;
-			//通过深入Document内部对body进行检测，获取窗口大小
-		   if (document.documentElement  && document.documentElement.clientHeight && document.documentElement.clientWidth)
-			{
-			winHeight = document.documentElement.clientHeight;
-			winWidth = document.documentElement.clientWidth;
-		    }
-			document.getElementsByClassName("vedio").style.height=9/16*winWidth;
-
-		   
-		   console.log(winWidth)
            this.get_case();
 		},
 		methods: {
@@ -67,7 +43,7 @@
 <style>
 .vedio-con{
 	width: 100%;
-	height:180px;
+	height:175px;
 }
 .vedio{
 	width: 90%;
