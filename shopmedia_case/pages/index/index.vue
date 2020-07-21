331751f5f5f338af2857ecb391a780d07c5f4abd
jlnotebook <904880305@qq.com>
2020-07-21 11:00:23 +0800

    jl

--- shopmedia_case/pages/index/index.vue
+++ shopmedia_case/pages/index/index.vue
@@ -1,11 +1,12 @@
 <template>
 	<view class="content">
-        <view class="vedio-con" v-for="value in adcase">
-			<video class="vedio" :poster="value.ad_cover" :src="value.ad_video" controls></video>
-			<view  class="vedio-text">泉塔大米</view>
+        <view class="vedio-con"  v-for="value in adcase">
+			<video class="vedio" :style="vedio_css" :poster="value.ad_cover" :src="value.ad_video" controls object-fit="fill"></video>
+			<view  class="vedio-text">{{value.ad_name}}</view>
 		</view>
 	</view>
 </template>
+
 
 <script>
 	export default {
@@ -14,14 +15,43 @@
 		},
 		data(){
 			return {
-               adcase:[] //广告案例列表
+               adcase:[] ,//广告案例列表
+			   vedio_css:{
+				   height:''
+			   }
 			}
 		},
 		onLoad(){
+			/**
+			 * 获取窗口宽度和高度
+			 */
+			var winWidth = 0;
+			var winHeight = 0;
 			
-           this.get_case();
+			//获取窗口宽度
+			 if (window.innerWidth)
+			winWidth = window.innerWidth;
+			 else if ((document.body) && (document.body.clientWidth))
+			winWidth = document.body.clientWidth;
+			//获取窗口高度
+			 if (window.innerHeight)
+			 winHeight = window.innerHeight;
+			else if ((document.body) && (document.body.clientHeight))
+			 winHeight = document.body.clientHeight;
+			 //通过深入Document内部对body进行检测，获取窗口大小
+			if (document.documentElement  && document.documentElement.clientHeight && document.documentElement.clientWidth)
+			 {
+			 winHeight = document.documentElement.clientHeight;
+			 winWidth = document.documentElement.clientWidth;
+			}
+            this.vedio_css.height=9/16*winWidth+'px';
+
+
+			//调用方法
+            this.get_case();
 		},
 		methods: {
+
 			/**
 			 * 获取广告案例
 			 */
@@ -43,15 +73,17 @@
 <style>
 .vedio-con{
 	width: 100%;
-	height:175px;
+	margin-top: 10px;
 }
 .vedio{
 	width: 90%;
-	margin: 5px 5%;
+	margin:0 5%;
 	height: 100%;
+	padding: 0;
 }
 .vedio-text{
 	width: 100%;
 	text-align: center;
+	font-size: 18px;
 }
 </style>
